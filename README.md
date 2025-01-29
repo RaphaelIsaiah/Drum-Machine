dfc0d50fe43c59f7ae5bfceb0dafa214fce70752
RaphaelIsaiah <rafvie12@gmail.com>
2025-01-29 11:56:55 +0100

    initialize drum machine project with basic structure and styling

---
+++ README.md
@@ -0,0 +1,2 @@
+# Drum Machine
+## A freeCodeCamp certification project
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Drum Machine</h1>
+    </>
+  );
+}
+
+export default App;
